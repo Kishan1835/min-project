78fea255ad0ef9f9eab860a8791f33b66a837134
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2025-06-14 22:47:07 +0530

    Download logic

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,15 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    const { userId, redirectToSignIn } = await auth();
+    if (!userId) {
+      return redirectToSignIn();
+    }
+  }
+});
 
 export const config = {
   matcher: [
